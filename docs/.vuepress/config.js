b3a66ab5901cb9405e6b994cd2dee75121b39f9e
Grazietta Hof <graziettahof@gmail.com>
2018-07-30 00:36:11 -0700

    basic file structure (#1)
    * basic file structure
    * moves navbar to left of page

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    themeConfig: {
+      nav: [
+        { text: 'Hello', link: '/hello' },
+        { text: 'Articles', link: '/articles' },
+        { text: 'Media', link: '/media' },
+      ]
+  
+    }
+  }
